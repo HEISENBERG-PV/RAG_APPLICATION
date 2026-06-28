bb47e7bf04871be77c5687d55287e8777017ac49
HEISENBERG <premvrgit@gmail.com>
2026-06-28 12:44:00 +0530

    Added RAG Pipelines

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-from langchain_community.document_loaders import TextLoader
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_chroma import Chroma
+
 load_dotenv()
 
+embedding = MistralAIEmbeddings(
+    model="mistral-embed"
+)
 
+vector_store = Chroma(
+    persist_directory="vector_DB",
+    embedding_function= embedding
+)
+
+retriever = vector_store.as_retriever(
+    search_type = "mmr",
+    search_kwargs = {
+        "k" : 3,
+        "fetch_k": 10,
+        "lambda_mult": 0.25
+    }
+)
+
+# doc = retriever.invoke("what is deep learning")
+# print(doc[0].page_content)
 
 template = ChatPromptTemplate.from_messages(
-    [("system", "You summarize the data in simple words"),
-     ("human", "{data}")
+    [
+        ("system", """You are powerful AI assistance
+        Answer the query only from the given context
+        If context doesn't have the answer then just tell user that the answer is not present in the document """
+    ),
+        ("human", """
+            Context: {context}
+            Query: {query}"""
+    )
      ]
 )
 
 model = init_chat_model(
     "gemini-2.5-flash-lite",
     model_provider="google_genai")
+
+
+while True:
+    query = input("You : ")
+    if query == "0":
+        break
+
+    doc = retriever.invoke(query)
+    context = " ".join(
+        [i.page_content for i in doc]
+    )
+
+    final_prompt = template.invoke({
+        "context": {context},
+        "query": {query}
+    })
+
+    res = model.invoke(final_prompt)
+
+    print("AI : ",res.content)
 
